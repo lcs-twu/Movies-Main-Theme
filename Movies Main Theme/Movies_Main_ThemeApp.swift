cdf9a5de0051e12d90da99fb4090320365336d71
Tom <twu@lcs.on.ca>
2023-04-19 11:51:26 -0400

    Initial Commit

---
+++ Movies Main Theme/Movies_Main_ThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_Main_ThemeApp.swift
+//  Movies Main Theme
+//
+//  Created by Tom Wu on 2023-04-19.
+//
+
+import SwiftUI
+
+@main
+struct Movies_Main_ThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
